f0e42a5f858498a30cf3a478fafea654d42a5bbf
Kristanto Sean <kristantosean@gmail.com>
2024-06-06 15:58:01 +0700

    Initial Commit

---
+++ BodyPoseImage/BodyPoseImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyPoseImageApp.swift
+//  BodyPoseImage
+//
+//  Created by Kristanto Sean on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BodyPoseImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
